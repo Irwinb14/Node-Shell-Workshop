e2823170f61d7500c6e048f138bd306f62342a20
jason <potvinj@gmail.com>
2022-11-10 14:27:11 -0500

    reformat bash.js

--- bash.js
+++ bash.js
@@ -1,14 +1,3 @@
-//Output a prompt
-// process.stdout.write("prompt > ");
+const pwd = require("./pwd");
 
-// The stdin 'data' event fires after a user types in a line
-
-// const pwdfunc = process.stdin.on("data", (data) => {
-//   const cmd = data.toString().trim(); //remove the newline
-//   if (cmd == "pwd") {
-//     process.stdout.write(process.cwd());
-//   }
-//   process.stdout.write("\nprompt > ");
-// });
-
-require("./pwd");
+pwd();
